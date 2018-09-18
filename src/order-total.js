b987dc6b9a1cb4e804161b988318585c6090bdc7
Ngela <ngela@Ngela-Pat-Yu.local>
2018-09-18 11:49:25 +0200

    order-total.*-Refactoring adding more jest functionality

--- src/order-total.js
+++ src/order-total.js
@@ -1,11 +1,12 @@
 function orderTotal(fetch, order) {
-    return fetch("http://localhost:3000/vat")
+    const sumOrders = order =>
+        order.items.reduce((accu, cur) =>
+            cur.price * (cur.quantity || 1) + accu
+            , 0);
+    return fetch('http://localhost:3000/vat')
         .then((data) => data.rates.standard.value)
-        .then((vat) => {
-            return order.items.reduce((accu, cur) => {
-                return cur.price * (cur.quantity || 1) + accu
-            }, 0) * (1 + vat / 100)
-        }
-        )
+        .then((vat) =>
+            sumOrders(order) * (1 + vat / 100)
+        );
 }
 module.exports = orderTotal;
--- src/order-total.test.js
+++ src/order-total.test.js
@@ -1,26 +1,39 @@
-const orderTotal = require('./order-total')
+const orderTotal = require('./order-total');
+const fakeFetchUrl = jest.fn((url) => {
+	expect(url).toBe('http://localhost:3000/vat');
+	return Promise.resolve({
+		rates: {
+			standard: {
+				value: 19
+			}
+		}
+	});
+});
 
-describe('orderTotal', () => {
-    fakeFetchUrl = (url) => {
-        expect(url).toBe('http://localhost:3000/vat');
-        return Promise.resolve({
-            rates: {
-                standard: {
-                    value: 19
-                }
-            }
-        })
-    }
-    it('sum total of orders', () => {
-        orderTotal(fakeFetchUrl, {
-            items: [{ name: 'Bag', quantity: 2, price: 20 }]
-        }).then(result => expect(result).toBe(20 * 2 * 1.19));
-    })
+describe("right orderTotal call", () => {
+	it('sum total of orders without quantity', () => {
+		orderTotal(fakeFetchUrl, {
+			items: [{ name: 'T-shirt', price: 40 }]
+		}).then(result => {
+			expect(result).toBe(40 * 1.19);
+			expect(fakeFetchUrl).toBeCalledWith('http://localhost:3000/vat');
+		});
 
-    it('sum total of orders without quantity', () => {
-        orderTotal(fakeFetchUrl, {
-            items: [{ name: 'T-shirt', price: 40 }]
-        }).then(result => expect(result).toBe(40 * 1.19));
-    })
+	})
 })
+describe('orderTotal', () => {
+	it('sum total of orders', () => {
+		orderTotal(fakeFetchUrl, {
+			items: [{ name: 'Bag', quantity: 2, price: 20 }]
+		}).then(result => {
+			expect(result).toBe(20 * 2 * 1.19);
+		});
+	});
+
+	it('sum total of orders without quantity', () => {
+		orderTotal(fakeFetchUrl, {
+			items: [{ name: 'T-shirt', price: 40 }]
+		}).then(result => expect(result).toBe(40 * 1.19));
+	});
+});
 
